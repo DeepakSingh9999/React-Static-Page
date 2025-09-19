13e787c962906bb47ec5fae0e0e1db5960110895
Deepak Singh <explorethe.unknowing536@passinbox.com>
2025-09-19 14:36:33 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,9 @@
+import Logo from '../assets/react.png'
 export default function Navbar(){
     return (
         <header>
             <nav>
-                <img src="../src/assets/react.svg" alt="React logo"></img>
+                <img src={Logo} alt="React logo"></img>
                 <span>React Facts</span>
             </nav>
         </header>
