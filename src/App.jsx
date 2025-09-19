7873a924febd9cb2384f92023345aa593603ca2c
Deepak Singh <Deepak.walton@proton.me>
2025-09-19 14:12:35 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Main from './components/Main.jsx'
+import Navbar from './components/Navbar.jsx'
+import Footer from './components/Footer.jsx'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    
+    <Navbar />
+    <Main />
+    <img src={reactLogo} className="logo react" alt="React logo" />
+    
+    <div className="line"></div>
+    <Footer />
+
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Main.jsx
@@ -0,0 +1,17 @@
+export default function Main(){
+    return(
+        <main>
+            <h1>Fun facts about React</h1>
+            <ul className="facts-list">
+                <li>Was first released in 2013.</li>
+                <li>Was originally created by Jordan Walke.</li>
+                <li>Has well over 200K stars on Github.</li>
+                <li>Is maintained by Meta.</li>
+                <li>Powers thousands of enterprise apps, including mobile apps.</li>
+                <li>React is built around components to make the UI into smaller reusable pieces.</li>
+                <li>React uses a one-way data binding, meaning data flows in a single direction: from parent to child components.</li>
+                <li>React uses JSX, a syntax extension that allows to write HTML-like code within Javascript.</li>
+            </ul>
+        </main>
+    )
+}
